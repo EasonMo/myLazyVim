3e44dcb760f3873084c73febea1ed802f265a5a7
EasonMo <moyicheng1990@gmail.com>
2024-07-22 03:03:38 +0800

    add case convert

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -46,12 +46,48 @@ return {
       "SudaWrite",
     },
   },
+  -- 成对符号处理
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
       require("nvim-surround").setup({})
+    end,
+  },
+  -- 驼峰命名转换
+  {
+    "gregorias/coerce.nvim",
+    tag = "v2.2",
+    event = "VeryLazy",
+    config = function()
+      local selector_m = require("coerce.selector")
+      local transformer_m = require("coerce.transformer")
+      local coroutine_m = require("coerce.coroutine")
+
+      require("coerce").setup({
+        modes = {
+          {
+            vim_mode = "n",
+            keymap_prefix = "<localleader>c",
+            selector = selector_m.select_current_word,
+            transformer = function(selected_region, apply)
+              return coroutine_m.fire_and_forget(
+                transformer_m.transform_lsp_rename_with_local_failover,
+                selected_region,
+                apply
+              )
+            end,
+          },
+        },
+      })
+      require("coerce").register_case({
+        keymap = "l",
+        case = function(str)
+          return vim.fn.tolower(str)
+        end,
+        description = "lowercase",
+      })
     end,
   },
 }
